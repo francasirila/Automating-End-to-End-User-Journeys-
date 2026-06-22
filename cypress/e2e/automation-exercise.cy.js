ce2dbdb329f20bd72dd8c10088d419ff50c0a166
francasirila <sirilaaotieno@gmail.com>
2026-06-22 19:07:02 +0300

    e-commerse website testing

--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -1,5 +1,19 @@
 describe('template spec', () => {
   it('passes', () => {
+    
     cy.visit('https://automationexercise.com')
+    cy.visit('https://automationexercise.com/')
+    cy.visit('https://automationexercise.com/login')
+    cy.visit('https://automationexercise.com/delete_account')
+    cy.visit('https://automationexercise.com/products')
+    cy.visit('https://automationexercise.com/products?search=dress')
+    cy.visit('https://automationexercise.com/product_details/3')
+    cy.visit('https://automationexercise.com/view_cart')
+    cy.visit('https://automationexercise.com/login')
+    cy.visit('https://automationexercise.com/view_cart')
+    cy.visit('https://automationexercise.com/checkout')
+    cy.visit('https://automationexercise.com/contact_us')
+    
+    cy.screenshot();
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.contains('Signup / Login').click()
+  cy.get('[data-qa="login-email"]').type(email)
+  cy.get('[data-qa="login-password"]').type(password)
+  cy.get('[data-qa="login-button"]').click()
+})
